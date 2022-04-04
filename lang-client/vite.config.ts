e71cbbbfeab6feb03b1f87abd17036a75a89406d
xiaoYown <qzy09101018@sina.com>
2022-04-04 11:59:35 +0800

    refactor(@njt-tools-open/lang-client): remove development warn

--- lang-client/vite.config.ts
+++ lang-client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  publicDir: './public',
   plugins: [solidPlugin()],
   build: {
     outDir: '../lang-cli/static',
